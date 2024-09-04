6a646473955f0ac228fbb4840752e1e60420778f
Ryan Jordan <34822768+rtjord@users.noreply.github.com>
2024-09-03 21:51:19 -0400

    added build and package scripts and updated README

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-## Put your name here:
+## Team Members:
 
+Trent Seaman, Ryan Jordan, Josh Wai, Pulkit Chhabra
 
-Trent Seaman,    
-Ryan Jordan,
-Josh Wai,
-Pulkit Chhabra
+## Getting Started
 
+1. Download and install Node.js found here: https://nodejs.org/en
+2. Download and install nvm found here: https://github.com/coreybutler/nvm-windows/releases
+3. Run `npm install` to install all dependencies
+4. Run `npm run build` to compile TypeScript to JavaScript
+5. Run `npm run package:<your_os>` to generate an executable file
+6. Run `npm run test` to test that the program is working.
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-const args = process.argv.slice(2);  // exclude first two arguments (node and script path)
+const args = process.argv.slice(2); // exclude first two arguments (node and script path)
 
-if (args.length !== 1) {  // check if exactly one argument is provided
-    console.log("Incorrect number of arguments provided");
-    process.exit(1);
-} else if (args[0] === "install") {  // check if the argument is "install"
-    console.log("Installing dependencies...");
-} else if (args[0] === "test") {  // check if the argument is "test"
+if (args.length !== 1) {
+  // check if exactly one argument is provided
+  console.log("Incorrect number of arguments provided");
+  process.exit(1);
+} else if (args[0] === "install") {
+  // check if the argument is "install"
+  console.log("Installing dependencies...");
+} else if (args[0] === "test") {
+  // check if the argument is "test"
   console.log("Running tests...");
-} else {  // otherwise, assume we have a URL_FILE
+} else {
+  // otherwise, assume we have a URL_FILE
   console.log("URL_FILE: " + args[0]);
 }
