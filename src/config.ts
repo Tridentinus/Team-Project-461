53d3fb7fb73f373165d88e3c7be980a301c1e2fb
Ryan Jordan <34822768+rtjord@users.noreply.github.com>
2024-09-14 22:00:09 -0400

    temporary fix until GITHUB_TOKEN is added to secrets

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+// TODO: Add GitHub token as a secret on the repository. Then remove the default value.
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN || 'dummyToken';
 
 if (!GITHUB_TOKEN) {
     throw new Error('GITHUB_TOKEN is required');
